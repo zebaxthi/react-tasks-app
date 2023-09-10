2580d7abb8f47f958bb0f9421617bde9b2ed71b3
Sebastian Aguirre <sebastian.aguirre@wiga.io>
2023-09-10 15:54:29 -0500

    tasks

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/tasks";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  function createTask(title, description) {
+    const newTask = {
+      title,
+      id: tasks.length + 1,
+      description: description,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        deleteTask,
+        createTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 1,
+    title: "primer tarea",
+    description: "ASD",
+  },
+  {
+    id: 2,
+    title: "segunda tarea",
+    description: "ASD",
+  },
+  {
+    id: 3,
+    title: "tercera tarea",
+    description: "ASD",
+  },
+];
